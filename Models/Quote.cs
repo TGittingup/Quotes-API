f07ad13714043247eee96afe67e2e34df8bb9d25
TGittingup <tayyib501@Hotmail.com>
2025-06-19 22:23:37 +0100

    Add files via upload

---
+++ Models/Quote.cs
@@ -0,0 +1,10 @@
+﻿namespace QuotesAPI.Models
+{
+    public class Quote
+    {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string Text { get; set; }
+
+    }
+}
